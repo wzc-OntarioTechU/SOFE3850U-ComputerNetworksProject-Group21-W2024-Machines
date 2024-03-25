54c5234cb7c47070e62dcd7e2aaacfc3c661a78b
GoldLink364 <goldlink364@Ubuntu.myguest.virtualbox.org>
2024-03-25 17:46:03 -0400

    server files

--- c-applications/upd_file_server.c
+++ c-applications/upd_file_server.c
@@ -1,5 +1,34 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int main(){
+struct pdu {
+	char type;
+	char data[100];
+};
+
+int main() {
+	int sd;
+	struct sockaddr_in serverAddr, clientAddr;
+	socklen_t addrSize = sizeof(clientAddr);
+
+	// Create UDP socket
+	sd = socket(AF_INET, SOCK_DGRAM, 0);
+
+	// Bind to server address
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(12345); // Server port
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	bind(sd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+	struct pdu rpdu;
+	recvfrom(sd, &rpdu, sizeof(rpdu), 0, (struct sockaddr*)&clientAddr, &addrSize);
+
+	printf("Received FILENAME PDU from client: %s\n", rpdu.data);
+    
+	close(sd);
+	return 0;
 }
+
